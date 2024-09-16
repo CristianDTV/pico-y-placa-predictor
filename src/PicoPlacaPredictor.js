e9511cc95701027b55be6a145aa7e6a2b4aeb416
CristianDTV <cristian.travez@epn.edu.ec>
2024-09-16 15:05:54 -0500

    Add basic structure of Pico y Placa methods

---
+++ src/PicoPlacaPredictor.js
@@ -0,0 +1,28 @@
+class PicoYPlacaPredictor {
+    constructor(licensePlate, dateString, timeString) {
+      this.licensePlate = licensePlate;
+      this.date = new Date(dateString);
+      this.time = this.parseTime(timeString);
+    }
+    
+   getLastDigit() {
+
+   }
+
+   getDayOfWeek() {
+
+   }
+
+   isInRestrictedHours() {
+
+   }
+
+   isRectricted() {
+
+   }
+
+   canBeOnRoad() {
+
+   }
+  }
+  
