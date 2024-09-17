577423652b90f931bc113604a54bf0b02a2e989b
CristianDTV <cristian.travez@epn.edu.ec>
2024-09-17 13:14:05 -0500

    Implementation of the logic and functionality of the methods

--- src/PicoPlacaPredictor.js
+++ src/PicoPlacaPredictor.js
@@ -1,28 +1,57 @@
 class PicoYPlacaPredictor {
-    constructor(licensePlate, dateString, timeString) {
-      this.licensePlate = licensePlate;
-      this.date = new Date(dateString);
-      this.time = this.parseTime(timeString);
-    }
-    
-   getLastDigit() {
-
-   }
-
-   getDayOfWeek() {
-
-   }
-
-   isInRestrictedHours() {
-
-   }
-
-   isRectricted() {
-
-   }
-
-   canBeOnRoad() {
-
-   }
+  constructor(licensePlate, dateString, timeString) {
+    this.licensePlate = licensePlate;
+    this.date = new Date(dateString);
+    this.time = this.parseTime(timeString);
   }
-  
+
+  parseTime(timeString) {
+    const [hours, minutes] = timeString.split(":").map(Number);
+    return { hours, minutes };
+  }
+
+  getLastDigit() {
+    return parseInt(this.licensePlate.slice(-1), 10);
+  }
+
+  getDayOfWeek() {
+    const day = this.date.getDay();
+    return day == 0 ? 7 : day;
+  }
+
+  isInRestrictedHours() {
+    const { hours, minutes } = this.time;
+    const totalMinutes = hours * 60 + minutes;
+
+    const morningStart = 7 * 60;
+    const morningEnd = 9 * 60 + 30;
+    const eveningStart = 16 * 60;
+    const eveningEnd = 19 * 60 + 30;
+
+    return (
+      (totalMinutes >= morningStart && totalMinutes <= morningEnd) ||
+      (totalMinutes >= eveningEnd && totalMinutes <= eveningEnd)
+    );
+  }
+
+  isRectricted() {
+    const restrictionSchedule = {
+      1: [1, 2],
+      2: [3, 4],
+      3: [5, 6],
+      4: [7, 8],
+      5: [9, 0],
+    };
+
+    const DayOfWeek = this.getDayOfWeek();
+    const LastDigit = this.getLastDigit();
+
+    const restrictedDigits = restrictionSchedule[DayOfWeek] || [];
+
+    return restrictedDigits.includes(LastDigit) && this.isInRestrictedHours();
+  }
+
+  canBeOnRoad() {
+    return !this.isRectricted();
+  }
+}
